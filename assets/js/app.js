e907b2912ff94eadf6e41796958993092204165e
jcheng305 <jcheng305@MacBook-Pro.T-mobile.com>
2017-08-22 06:48:24 -0700

    More Changes made to portfolio

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,6 +21,7 @@ var data = {
 
 
 $(document).on('ready', function() {
+  $("body").fadeIn(2000);
   $(".first_name").text(data.first_name);
   $(".occupation").text(data.occupation);
   $(".location").text(data.location);
@@ -33,3 +34,25 @@ $(document).on('ready', function() {
   }
   $(".img-responsive").attr("src",data.profile_image);
 });
+
+// //for tab function
+// function openCity(evt, cityName) {
+//     // Declare all variables
+//     var i, tabcontent, tablinks;
+//
+//     // Get all elements with class="tabcontent" and hide them
+//     tabcontent = document.getElementsByClassName("tabcontent");
+//     for (i = 0; i < tabcontent.length; i++) {
+//         tabcontent[i].style.display = "none";
+//     }
+//
+//     // Get all elements with class="tablinks" and remove the class "active"
+//     tablinks = document.getElementsByClassName("tablinks");
+//     for (i = 0; i < tablinks.length; i++) {
+//         tablinks[i].className = tablinks[i].className.replace(" active", "");
+//     }
+//
+//     // Show the current tab, and add an "active" class to the button that opened the tab
+//     document.getElementById(cityName).style.display = "block";
+//     evt.currentTarget.className += " active";
+// }
